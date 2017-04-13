6d184a7487b087623089dacec4ded7df3d9c211e
otofu-square <yukita@feedforce.jp>
2017-04-13 23:31:56 +0900

    :+1: Add actions

--- redux-tutorial/src/reducers/bar.js
+++ redux-tutorial/src/reducers/bar.js
@@ -1,9 +1,30 @@
 import Immutable from 'immutable'
 import type { fromJS as Immut } from 'immutable'
-import { handleActions } from 'redux-actions'
 
-const initialState = Immutable.fromJS({})
+import {
+  CLICK_ACTION_CI,
+  CLICK_ACTION_S,
+  CLICK_ACTION_T,
+  CLICK_ACTION_CR,
+} from '../actions/hoge-action'
 
-const bar = handleActions(initialState, 0)
+const initialState = Immutable.fromJS({
+  message: 'Initial message'
+})
+
+const barReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CLICK_ACTION_CI:
+      return state.set('message', 'CI')
+    case CLICK_ACTION_S:
+      return state.set('message', 'S')
+    case CLICK_ACTION_T:
+      return state.set('message', 'T')
+    case CLICK_ACTION_CR:
+      return state.set('message', 'CR')
+    default:
+      return state
+  }
+}
 
 export default bar
