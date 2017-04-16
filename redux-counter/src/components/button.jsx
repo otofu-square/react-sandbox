7d2348a38732de96e76218d51b3121ea9979de01
otofu-square <yukita@feedforce.jp>
2017-04-16 23:49:32 +0900

    :construction: Add components, containers

---
+++ redux-counter/src/components/button.jsx
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react'
+
+type Props = {
+  label: string,
+  onClick: Function,
+}
+
+const Button = ({ label, onClick }: Props) =>
+  <button>
+
+  </button>
+
+export default Button
