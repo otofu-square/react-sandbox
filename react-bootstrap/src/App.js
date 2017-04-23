e0d31fad8646b9d0e2359b1ddad5756e2f04e1f4
otofu-square <yukita@feedforce.jp>
2017-04-23 15:11:21 +0900

    :+1: Add various coloful buttons

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -2,12 +2,29 @@
 
 import React from 'react'
 
-import DefaultButton from './components/buttons/default-button'
+import { ButtonToolbar } from 'react-bootstrap'
+import {
+  DefaultButton,
+  PrimaryButton,
+  SuccessButton,
+  InfoButton,
+  WarningButton,
+  DangerButton,
+  LinkButton,
+} from './components/buttons'
 import './App.css'
 
 const App = () =>
   <div>
-    <DefaultButton className="a" label="Hello World Button"/>
+    <ButtonToolbar>
+      <DefaultButton className="a" label="Default Button"/>
+      <PrimaryButton className="a" label="Primary Button"/>
+      <SuccessButton className="a" label="Success Button"/>
+      <InfoButton className="a" label="Info Button"/>
+      <WarningButton className="a" label="Warning Button"/>
+      <DangerButton className="a" label="Danger Button"/>
+      <LinkButton className="a" label="Link Button"/>
+    </ButtonToolbar>
   </div>
 
 export default App
