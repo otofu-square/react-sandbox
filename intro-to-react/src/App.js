fd60c38b9d44d2dd0a5f2f88b88e88587a363334
otofu-square <yukita@feedforce.jp>
2017-04-24 22:46:17 +0900

    :+1: Add declare UI

--- intro-to-react/src/App.js
+++ intro-to-react/src/App.js
@@ -28,7 +28,20 @@ class WeatherDisplay extends Component {
   render() {
     const weatherData = this.state.weatherData
     if (!weatherData) return <div>Loading</div>
-    return <div>{JSON.stringify(weatherData)}</div>
+    const weather = weatherData.weather[0]
+    const iconUrl = `http://openweathermap.org/img/w/${weather.icon}.png`
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description}/>
+        </h1>
+        <p>Current: {weatherData.main.temp}</p>
+        <p>High: {weatherData.main.temp_max}</p>
+        <p>Low: {weatherData.main.temp_min}</p>
+        <p>Wind Speed: {weatherData.wind.speed}</p>
+      </div>
+    )
   }
 }
 
