634dfc4cc0db8e90f682fca97baa164b0f68ba2f
otofu-square <yukita@feedforce.jp>
2017-04-24 22:11:50 +0900

    :+1: Add simple WearherDisplay component

--- intro-to-react/src/App.js
+++ intro-to-react/src/App.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class WeatherDisplay extends Component {
+  render() {
+    return (
+      <h1>Displaying weather for city {this.props.zip}</h1>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WeatherDisplay zip={"12345"}/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
