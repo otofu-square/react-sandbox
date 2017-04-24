7fbfab93b22d8755cefaa47384c13386481f73ed
otofu-square <yukita@feedforce.jp>
2017-04-24 22:16:58 +0900

    :+1: Add a little bit complecated component

--- intro-to-react/src/App.js
+++ intro-to-react/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const PLACES = [
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" }
+]
+
 class WeatherDisplay extends Component {
   render() {
     return (
@@ -15,6 +22,11 @@ class App extends Component {
     return (
       <div className="App">
         <WeatherDisplay zip={"12345"}/>
+        {PLACES.map((place, index) => (
+          <button key={index} onClick={() => console.log(`Clicked index ${index}`)}>
+            {place.name}
+          </button>
+        ))}
       </div>
     )
   }
