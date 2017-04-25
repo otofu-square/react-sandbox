a3dfb736080f8939f951606865c62b6376836c94
otofu-square <yukita@feedforce.jp>
2017-04-26 01:34:56 +0900

    :+1: Transform reusable components

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import Water from './components/water'
+import Liquid from './components/Liquid'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Water/>
+        <Liquid name="Ethaanol" freezing={-173.2} boiling={173.1}/>
       </div>
     )
   }
