d81b5bf9fa0ce1716b3f2b5bbec3a20ca5cbab1d
otofu-square <yukita@feedforce.jp>
2017-04-26 21:37:36 +0900

    :+1: Try functional component

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
+const FunctionalComponent = () =>
+  <div>
+    <h1>Hello, World</h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+  </div>
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, World</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
+      <FunctionalComponent />
     )
   }
 }
--- react-sandbox/src/index.js
+++ react-sandbox/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
-const render = () =>
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  )
-
-setInterval(render, 1000)
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
