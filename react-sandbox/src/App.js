f51c14dfbfe14f81389237a67d7eddb7b6b9ea0c
otofu-square <yukita@feedforce.jp>
2017-04-26 21:28:30 +0900

    :clock: Clock

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 
-import Greeting from './components/Greeting'
-
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello, World</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </div>
     )
   }
