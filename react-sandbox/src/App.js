c878ed5aaf90c2c898bbf78377f969085617e25d
otofu-square <yukita@feedforce.jp>
2017-04-26 22:37:33 +0900

    :+1: Add Clock component

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -1,39 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-const formatDate = (date) => date.toLocaleDateString()
+import Clock from './components/Clock'
 
-const Avater = (props) =>
-  <img
-    src="props.user.avaterUrl"
-    alt="props.user.name"
-    className="Avatar"
-  />
-
-const UserInfo = (props) =>
-  <div className="UserInfo">
-    <Avater user={props.user} />
-    <div className="UserInfo-name">
-      {props.user.name}
-    </div>
+const App = () =>
+  <div>
+    <Clock />
+    <Clock />
+    <Clock />
   </div>
-
-const Comment = (props) =>
-  <div className="Comment">
-    <UserInfo user={props.author} />
-    <div className="Comment-text">
-      {props.text}
-    </div>
-    <div className="Comment-date">
-      {formatDate(props.data)}
-    </div>
-  </div>
-
-class App extends Component {
-  render() {
-    return (
-      <Comment author={{ author: name }} text="This is a text" data={new Date()}/>
-    )
-  }
-}
 
 export default App
