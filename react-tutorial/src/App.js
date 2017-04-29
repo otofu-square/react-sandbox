7af85700399cdf0c9b72998c7fa8886269c8ed5d
otofu-square <yukita@feedforce.jp>
2017-04-29 15:34:08 +0900

    :+1: Add starter codes

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
 
-class App extends Component {
-  render() {
-  }
-}
+import Game from './components/Game'
+
+const App = () =>
+  <Game />
 
 export default App
