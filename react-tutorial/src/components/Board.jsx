c070d8f7b4f6aebccc7bf6a7b3fdbb9d3a1e9a38
otofu-square <yukita@feedforce.jp>
2017-04-29 15:55:09 +0900

    :+1: Add state to Square component

--- react-tutorial/src/components/Board.jsx
+++ react-tutorial/src/components/Board.jsx
@@ -4,7 +4,7 @@ import Square from './Square'
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />
+    return <Square value={i}/>
   }
   render() {
     const status = 'Next player: X'
