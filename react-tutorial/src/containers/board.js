d9c15800b0c98d28fb2a8bc1cc66ad6e4a4a3196
otofu-square <yukita@feedforce.jp>
2017-04-29 18:49:24 +0900

    :+1: Add containers

---
+++ react-tutorial/src/containers/board.js
@@ -0,0 +1,19 @@
+// @flow
+
+import { connect } from 'react-redux'
+
+import Board from '../components/Board'
+import { toggleSquare } from '../actions/action'
+
+const mapStateToProps = state => ({
+  status: state.status,
+  squares: state.squares,
+})
+
+const mapDispatcherToProps = dispatch => ({
+  handleClick: (i) => {
+    dispatch(toggleSquare(i))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatcherToProps)(Board)
