f5677b39d62fc1e02f554c4c9c1c3dcc6fe1ec77
otofu-square <yukita@feedforce.jp>
2017-04-29 18:51:45 +0900

    :+1: Add Provider component by react-redux

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import './styles/index.css'
 import App from './App'
+import boardReducer from './reducers/board'
+
+const store = createStore(boardReducer)
+
+const Root = () =>
+  <Provider store={store}>
+    <App />
+  </Provider>
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
 )
