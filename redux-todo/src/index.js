a84a98db3da8e607b533fee7e8e32b01ced63adc
otofu-square <yukita@feedforce.jp>
2017-05-13 18:46:05 +0900

    :shirt: Formatting by eslint

--- redux-todo/src/index.js
+++ redux-todo/src/index.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App'
+import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+/* eslint-disable react/jsx-filename-extension */
+ReactDOM.render(<App />, document.getElementById('root'));
