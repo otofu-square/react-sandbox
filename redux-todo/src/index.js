c1dc50b8e02628f9f7a15930936e2adb44f71bf6
otofu-square <yukita@feedforce.jp>
2017-05-14 23:48:20 +0900

    :+1: Add store

--- redux-todo/src/index.js
+++ redux-todo/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import todoReducer from './reducers/todoReducer';
+import visibilityFilterReducer from './reducers/visibilityFilterReducer';
+
+const reducers = combineReducers(todoReducer, visibilityFilterReducer);
+const store = createStore(reducers);
 
 /* eslint-disable react/jsx-filename-extension */
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
