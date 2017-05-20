8680f758244c52ccf32a5fac05f5aa3b2effd6dd
otofu-square <yukita@feedforce.jp>
2017-05-20 17:32:49 +0900

    :+1: Add ejected-create-react-app

---
+++ ejected-create-react-app/src/index.js
@@ -0,0 +1,9 @@
+// @flow
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+/* eslint-disable react/jsx-filename-extension */
+ReactDOM.render(<App />, document.getElementById('root'));
