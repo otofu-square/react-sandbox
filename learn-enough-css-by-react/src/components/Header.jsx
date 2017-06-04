c5add5bc7a1b3250a99ecd1051fa1425ab6523ff
otofu-square <yukita@feedforce.jp>
2017-06-04 23:03:31 +0900

    :+1: Add header menu

--- learn-enough-css-by-react/src/components/Header.jsx
+++ learn-enough-css-by-react/src/components/Header.jsx
@@ -4,10 +4,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  background-color: #f7f2b4;
+  background-color: #26d0c9;
   height: 90px;
 `;
 
-const Header = () => <Container />;
+const HeaderLogo = styled.div`
+  font-size: 36px;
+  float: left;
+`;
+
+const HeaderList = styled.div`
+  float: left;
+`;
+
+const Li = styled.li`
+  list-style: none;
+  float: left;
+`;
+
+const Header = () =>
+  <Container>
+    <HeaderLogo>
+      Progate
+    </HeaderLogo>
+    <HeaderList>
+      <ul>
+        <Li>プログラミングとは</Li>
+        <Li>学べるレッスン</Li>
+        <Li>お問い合わせ</Li>
+      </ul>
+    </HeaderList>
+  </Container>;
 
 export default Header;
