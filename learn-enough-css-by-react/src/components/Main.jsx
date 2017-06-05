10b98f5fadea100feaf3bbec9545fbed9040821f
otofu-square <yukita@feedforce.jp>
2017-06-05 23:51:05 +0900

    :+1: Add Contents component

--- learn-enough-css-by-react/src/components/Main.jsx
+++ learn-enough-css-by-react/src/components/Main.jsx
@@ -23,9 +23,28 @@ const CopySpan = styled.span`
 `;
 
 const Contents = styled.div`
+  height: 500px;
+  margin-top: 100px;
+`;
+
+const ContentsHeader3 = styled.h3`
+  border-bottom: 2px solid #dee7ec;
+  font-size: 28px;
+  padding-bottom: 15px;
+  margin-bottom: 50px;
+`;
+
+const ContentsParagraph = styled.p`
+  font-size: 24px;
+  margin-top: 30px;
 `;
 
 const ContactForm = styled.div`
+`;
+
+const ContentItem = styled.div`
+  float: left;
+  margin-right: 40px;
 `;
 
 const Main = () =>
@@ -35,7 +54,35 @@ const Main = () =>
       <CopyHeader2>プログラミングの世界へようこそ</CopyHeader2>
     </Copy>
     <Contents>
-      This is contents.
+      <ContentsHeader3>学べるレッスン</ContentsHeader3>
+      <ContentItem>
+        <img
+          src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/html.svg"
+          alt="HTML & CSS"
+        />
+        <p>HTML & CSS</p>
+      </ContentItem>
+      <ContentItem>
+        <img
+          src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/php.svg"
+          alt="PHP"
+        />
+        <p>PHP</p>
+      </ContentItem>
+      <ContentItem>
+        <img
+          src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/ruby.svg"
+          alt="Ruby"
+        />
+        <p>Ruby</p>
+      </ContentItem>
+      <ContentItem>
+        <img
+          src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/swift.svg"
+          alt="Swift"
+        />
+        <p>Swift</p>
+      </ContentItem>
     </Contents>
     <ContactForm>
       This is contact from.
