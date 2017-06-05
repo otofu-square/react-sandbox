ddb78ca1c5f4b78eca2a91a2e7cde4e3fbd4967d
otofu-square <yukita@feedforce.jp>
2017-06-05 23:32:10 +0900

    :+1: Add CopyContainer components

--- learn-enough-css-by-react/src/components/Main.jsx
+++ learn-enough-css-by-react/src/components/Main.jsx
@@ -4,10 +4,42 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  background-color: #bdf7f1;
-  height: 600px;
+  padding: 100px 80px;
 `;
 
-const Main = () => <Container />;
+const Copy = styled.div`
+`;
+
+const CopyHeader1 = styled.h1`
+  font-size: 140px;
+`;
+
+const CopyHeader2 = styled.h2`
+  font-size: 60px;
+`;
+
+const CopySpan = styled.span`
+  color: #ff4a4a;
+`;
+
+const Contents = styled.div`
+`;
+
+const ContactForm = styled.div`
+`;
+
+const Main = () =>
+  <Container>
+    <Copy>
+      <CopyHeader1>HELLO WORLD<CopySpan>.</CopySpan></CopyHeader1>
+      <CopyHeader2>プログラミングの世界へようこそ</CopyHeader2>
+    </Copy>
+    <Contents>
+      This is contents.
+    </Contents>
+    <ContactForm>
+      This is contact from.
+    </ContactForm>
+  </Container>;
 
 export default Main;
