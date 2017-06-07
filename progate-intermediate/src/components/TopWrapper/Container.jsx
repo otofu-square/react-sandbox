090ff8bd75ef1236de02cb2b240acb1675608a88
otofu-square <yukita@feedforce.jp>
2017-06-08 00:02:03 +0900

    :+1: Improve TopWrapper components

---
+++ progate-intermediate/src/components/TopWrapper/Container.jsx
@@ -0,0 +1,26 @@
+// @flow
+
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleWrapper = styled.div`
+  width: 1170px;
+  padding: 0 15px 0 15px;
+  margin: 0 auto;
+`;
+
+const Catchcopy = styled.h1`
+`;
+
+const Message = styled.p`
+`;
+
+const Container = () =>
+  <StyleWrapper>
+    <Catchcopy>LEARN TO CODE.</Catchcopy>
+    <Catchcopy>LEARN TO BE CREATIVE.</Catchcopy>
+    <Message>Progateはオンラインプログラミング学習サービスです。</Message>
+    <Message>初心者にもやさしいスライドとレッスンで、ウェブサービスを作りながらプログラミングを学んでいきましょう。</Message>
+  </StyleWrapper>;
+
+export default Container;
--- progate-intermediate/src/components/TopWrapper/index.js
+++ progate-intermediate/src/components/TopWrapper/index.js
@@ -3,6 +3,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const TopWrapper = () => <div />;
+import Container from './Container';
+
+const StyleWrapper = styled.div`
+  padding: 180px 0 100px 0;
+  background-image: url(https://prog-8.com/images/html/advanced/top.png);
+  background-size: cover;
+  color: white;
+`;
+
+const TopWrapper = () =>
+  <StyleWrapper>
+    <Container />
+  </StyleWrapper>;
 
 export default TopWrapper;
