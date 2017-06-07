fa3fa7003b17319ec169ef82ab2ff20c238277e0
otofu-square <yukita@feedforce.jp>
2017-06-07 22:41:43 +0900

    :recycle: Refactor components

--- progate-intermediate/src/components/Footer/index.js
+++ progate-intermediate/src/components/Footer/index.js
@@ -2,3 +2,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
+
+const Footer = () => <footer />;
+
+export default Footer;
--- progate-intermediate/src/components/LessonWrapper/index.js
+++ progate-intermediate/src/components/LessonWrapper/index.js
@@ -2,3 +2,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
+
+const LessonWrapper = () => <div />;
+
+export default LessonWrapper;
