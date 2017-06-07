88855bc6802a036978723af9c9c251eaeaaed824
otofu-square <yukita@feedforce.jp>
2017-06-07 22:34:54 +0900

    :+1: Add base components

--- progate-intermediate/src/App.js
+++ progate-intermediate/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Header from './components/Header/index';
+import TopWrapper from './components/TopWrapper/index';
+import LessonWrapper from './components/LessonWrapper/index';
+import MessageWrapper from './components/MessageWrapper/index';
+import Footer from './components/Footer/index';
+
 const App = () =>
   <div>
-    <h1>Hello World!!</h1>;
-    <h2>This is hello world page.</h2>;
+    <Header />
+    <TopWrapper />
+    <LessonWrapper />
+    <MessageWrapper />
+    <Footer />
   </div>;
 
 export default App;
