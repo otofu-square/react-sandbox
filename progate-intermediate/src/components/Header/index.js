0d4882e9041c65132911ef2c958e16769b5042f6
otofu-square <yukita@feedforce.jp>
2017-06-10 20:27:37 +0900

    :+1: Add login button

--- progate-intermediate/src/components/Header/index.js
+++ progate-intermediate/src/components/Header/index.js
@@ -9,14 +9,35 @@ const StyleWrapper = styled.header`
   background-color: rgba(34, 49, 52, 0.9);
 `;
 
+const LeftWrapper = styled.div`
+  float: left;
+`;
+
+const RightWrapper = styled.div`
+  float: right;
+  background-color: rgba(255, 255, 255, 0.3);
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+`;
+
 const Logo = styled.img`
   width: 124px;
   margin-top: 20px;
 `;
 
+const LoginButton = styled.a`
+`;
+
 const Header = () =>
   <StyleWrapper>
-    <Logo src="https://prog-8.com/images/html/advanced/main_logo.png" />
+    <LeftWrapper>
+      <Logo src="https://prog-8.com/images/html/advanced/main_logo.png" />
+    </LeftWrapper>
+    <RightWrapper>
+      <LoginButton href="#">ログイン</LoginButton>
+    </RightWrapper>
   </StyleWrapper>;
 
 export default Header;
