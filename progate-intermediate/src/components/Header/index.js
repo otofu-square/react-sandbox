1aeb600d01c1f7d70922fab0cd14a5c20c76006b
otofu-square <yukita@feedforce.jp>
2017-06-10 20:15:35 +0900

    :+1: Add header logo

--- progate-intermediate/src/components/Header/index.js
+++ progate-intermediate/src/components/Header/index.js
@@ -3,6 +3,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Header = () => <header />;
+const StyleWrapper = styled.header`
+  height: 65px;
+  width: 100%;
+  background-color: rgba(34, 49, 52, 0.9);
+`;
+
+const Logo = styled.img`
+  width: 124px;
+  margin-top: 20px;
+`;
+
+const Header = () =>
+  <StyleWrapper>
+    <Logo src="https://prog-8.com/images/html/advanced/main_logo.png" />
+  </StyleWrapper>;
 
 export default Header;
