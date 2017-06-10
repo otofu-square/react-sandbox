1ceb7f759c817844b838488bee8a5e2de42044ce
otofu-square <yukita@feedforce.jp>
2017-06-10 21:00:12 +0900

    :+1: Add styles for Lessons components

--- progate-intermediate/src/components/LessonWrapper/index.js
+++ progate-intermediate/src/components/LessonWrapper/index.js
@@ -3,6 +3,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const LessonWrapper = () => <div />;
+const StyleWrapper = styled.div`
+  height: 500px;
+  padding-bottom: 80px;
+  background-color: #f7f7f7;
+`;
+
+const Heading = styled.div`
+  padding-top: 60px;
+  padding-bottom: 30px;
+  color: #5f5d60;
+
+  h2 {
+    font-weight: normal;
+  }
+`;
+
+const Lessons = styled.div`
+
+`;
+
+const LessonWrapper = () =>
+  <StyleWrapper>
+    <Heading>Learn Where to Get Started!</Heading>
+    <Lessons />
+  </StyleWrapper>;
 
 export default LessonWrapper;
