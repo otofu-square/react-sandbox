5a82bb785fbddd3783e07a18cbdf819ea4bdf6b5
otofu-square <yukita@feedforce.jp>
2017-06-11 22:15:49 +0900

    :+1: Add Footer component

--- progate-intermediate/src/components/Footer/index.js
+++ progate-intermediate/src/components/Footer/index.js
@@ -3,6 +3,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Footer = () => <footer />;
+const StyleWrapper = styled.div`
+  padding-top: 30px;
+`;
+
+const Logo = styled.img`
+  width: 125px;
+`;
+
+const Message = styled.p`
+  color: #b3aeb5;
+  font-size: 12px;
+`;
+
+const Footer = () =>
+  <StyleWrapper>
+    <Logo src="https://prog-8.com/images/html/advanced/footer_logo.png" />
+    <Message>Learn to Code,Learn to be Creative.</Message>
+  </StyleWrapper>;
 
 export default Footer;
