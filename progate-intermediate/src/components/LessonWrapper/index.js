c1d3a930b3cbb61578f4b0bf3b449cb1358fb18a
otofu-square <yukita@feedforce.jp>
2017-06-11 19:44:58 +0900

    :recycle: Refactor by adding Heading component

--- progate-intermediate/src/components/LessonWrapper/index.js
+++ progate-intermediate/src/components/LessonWrapper/index.js
@@ -3,29 +3,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Heading from './Heading';
+
 const StyleWrapper = styled.div`
   height: 500px;
   padding-bottom: 80px;
   background-color: #f7f7f7;
 `;
 
-const Heading = styled.div`
-  padding-top: 60px;
-  padding-bottom: 30px;
-  color: #5f5d60;
-
-  h2 {
-    font-weight: normal;
-  }
-`;
-
 const Lessons = styled.div`
-
 `;
 
 const LessonWrapper = () =>
   <StyleWrapper>
-    <Heading>Learn Where to Get Started!</Heading>
+    <Heading text="Learn Where to Get Started!" />
     <Lessons />
   </StyleWrapper>;
 
