8adecbc48b0175dfb29bd1365388f9a6fcfea9f0
otofu-square <yukita@feedforce.jp>
2017-11-29 00:17:04 +0900

    :wrench: Modify files

--- react-google-map/src/index.js
+++ react-google-map/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// @flow
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('Root element is not exist.');
+}
+
+ReactDOM.render(<App />, root);
 registerServiceWorker();
