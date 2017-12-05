fb08bd1d8141bc12a4a9fecd2f86d2fcced78bee
otofu-square <yukita@feedforce.jp>
2017-12-05 21:17:33 +0900

    :recycle: Add flow typings

--- formik-sandbox/src/index.jsx
+++ formik-sandbox/src/index.jsx
@@ -1,8 +1,12 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+if (!root) throw new Error('Root element is not exist.');
+
+ReactDOM.render(<App />, root);
 registerServiceWorker();
