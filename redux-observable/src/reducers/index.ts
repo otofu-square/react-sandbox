92ffa597589e51ba0ff814623e030c709d0eb984
otofu-square <yukita@feedforce.jp>
2017-12-18 09:17:27 +0900

    :+1: Add reducers

---
+++ redux-observable/src/reducers/index.ts
@@ -0,0 +1,27 @@
+import {
+  FETCH_REQUEST,
+  FETCH_FAILURE,
+  FETCH_SUCCESS,
+  Action,
+} from '../actions';
+import { State } from '../types';
+
+const initialState = {
+  userId: 0,
+  id: 0,
+  title: 'null',
+  body: 'null',
+};
+
+export const reducer = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case FETCH_REQUEST:
+      return state;
+    case FETCH_FAILURE:
+      return state;
+    case FETCH_SUCCESS:
+      return { ...state, userId: action.payload.userId };
+    default:
+      return state;
+  }
+};
