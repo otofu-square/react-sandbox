7d31243e8fdd05279837cebb66be824febf17a18
otofu-square <yukita@feedforce.jp>
2018-02-04 11:58:23 +0900

    :+1: Add globalStyles.ts

--- progate-begginer/src/index.tsx
+++ progate-begginer/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 
 import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './globalStyles';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
