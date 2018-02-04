4c28ce455af169d4885b17d1d63b4cdac7a8c0b4
otofu-square <yukita@feedforce.jp>
2018-02-04 22:54:28 +0900

    :wrench: Add header-logo, header-list

--- progate-begginer/src/components/Header/index.tsx
+++ progate-begginer/src/components/Header/index.tsx
@@ -1,6 +1,32 @@
+import * as React from 'react';
 import styled from 'styled-components';
 
-export const Header = styled.div`
+const StyledDiv = styled.div`
   background-color: #f7f2b4;
   height: 90px;
 `;
+
+const HeaderLogo = styled.div`
+  font-size: 36px;
+  float: left;
+`;
+
+const HeaderList = styled.ul`
+  float: left;
+
+  & > li {
+    list-style: none;
+    float: left;
+  }
+`;
+
+export const Header = () => (
+  <StyledDiv>
+    <HeaderLogo>Progate</HeaderLogo>
+    <HeaderList>
+      <li>プログラミングとは</li>
+      <li>学べるレッスン</li>
+      <li>お問い合わせ</li>
+    </HeaderList>
+  </StyledDiv>
+);
