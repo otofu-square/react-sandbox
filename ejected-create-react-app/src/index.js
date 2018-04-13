b057b0011ac0e4d9e3eb2950ee02a1e53cfc230a
otofu-square <yukita@feedforce.jp>
2018-04-13 17:59:03 +0900

    :wrench: $ rm -rf ejected-create-react-app && create-react-app ejected-create-react-app

--- ejected-create-react-app/src/index.js
+++ ejected-create-react-app/src/index.js
@@ -1,9 +1,8 @@
-// @flow
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-/* eslint-disable react/jsx-filename-extension */
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
